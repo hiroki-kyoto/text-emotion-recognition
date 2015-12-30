e3c7bb148c7c8216ec546ad767cdb9da1165240c
xiangchao027 <xiangchao027@gmail.com>
2015-12-30 15:41:01 +0800

    compute information gain of all terms

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # 1. compute the information gain
-# 2. 
+# 2. extract the feature out
+# 3. using neural network to train the model
+# 4. test and validation of model 
 import os
+import math
 
 cmtdir = 'comments/'
 path_pos = 'haopingfenci.txt'
 path_neg = 'chapingfenci.txt'
+file_terms = 'terms.txt'
+file_trm_pos = 'trm_pos.txt'
+file_trm_neg = 'trm_neg.txt'
 
 def _main_() :
 	cmt_pos = 0 # count of positive comment
@@ -81,10 +87,77 @@ def _main_() :
 	# finally to show the statistical data
 	print 'postive total: ',  cmt_pos
 	print 'negtive total: ', cmt_neg
-	print '####################################'
+	
+	# save terms into files
+	f = open(file_terms, 'w')
 	for i in terms :
-		print i
-	print 'term of positive : ', trm_pos
-	print 'term of negtive : ', trm_neg
+		f.write(i + '\t')
+	f.close()
+	f = open(file_trm_pos, 'w')
+	for i in trm_pos :
+		f.write(str(i) + '\t')
+	f.close()
+	f = open(file_trm_neg, 'w')
+	for i in trm_neg :
+		f.write(str(i) + '\t')
+	f.close()
+
+	# now calculate the information gain
+	cmt_tot = float(cmt_pos + cmt_neg)
+	ppos = cmt_pos/cmt_tot
+	pneg = 1.0 - ppos
+	Hpos = -1.0*ppos*(math.log(ppos,2))
+	Hneg = -1.0*pneg*(math.log(pneg,2))
+	Htot = float(Hpos + Hneg)
+
+	IGterms = range(0, len(terms))
+	for i in range(0, len(terms)) :
+		trm_tot = float(trm_pos[i] + trm_neg[i])
+		pterm = trm_tot/cmt_tot
+
+		if trm_pos[i]==0 :
+			Htrm_pos = 0.0
+		else :
+			pterm_pos = trm_pos[i]/trm_tot
+			Htrm_pos = -1.0*pterm_pos* \
+			math.log(pterm_pos)/math.log(2)
+		if trm_neg[i]==0 :
+			Htrm_neg = 0.0
+		else :
+			pterm_neg = trm_neg[i]/trm_tot
+			Htrm_neg = -1.0*pterm_neg* \
+			math.log(pterm_neg)/math.log(2)
+		
+		ntrm_tot = float(cmt_tot - trm_tot)
+		ntrm_pos = float(cmt_pos - trm_pos[i])
+		ntrm_neg = float(cmt_neg - trm_neg[i])
+		if ntrm_tot-ntrm_pos-ntrm_neg!=0 :
+			print 'PROGRAM RUNNING STATUS :\
+			ERROR IN YOUR LOGIC!'
+			return 1
+		
+		if ntrm_pos==0 :
+			Hntrm_pos = 0.0
+		else :
+			pnterm_pos = ntrm_pos/ntrm_tot
+			Hntrm_pos = -1.0*pnterm_pos* \
+			math.log(pnterm_pos)/math.log(2)
+		if ntrm_neg==0 :
+			Hntrm_neg = 0.0
+		else :
+			pnterm_neg = ntrm_neg/ntrm_tot
+			Hntrm_neg = -1.0*pnterm_neg* \
+			math.log(pnterm_neg)/math.log(2)
+
+		# calculate information gain with or without terms[i]
+		IGterms[i] = -1.0*pterm*(Htrm_pos+Htrm_neg) \
+		- (1-pterm)*(Hntrm_pos+Hntrm_neg)
+		IGterms[i] = Htot - IGterms[i]
+		
+	# show the IG
+	f = open('information_gain.txt', 'w')
+	for i in IGterms :
+		f.write(str(i) + '\t')
+	f.close()
 
 _main_();
