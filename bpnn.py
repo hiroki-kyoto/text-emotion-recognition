b822a6828565541af527d296265a01cf5e9d0222
xiangchao027 <xiangchao027@gmail.com>
2015-12-31 18:11:38 +0800

    implement the method of rand_train for BPNN

--- bpnn.py
+++ bpnn.py
@@ -15,38 +15,40 @@ class BPNN(object) :
 			]
 		learning_rate = 0.5 by default
 	Methods:
-		void train(data, feedback) - data should have
+		void rand_train(data, feedback, times) - data should have
 			dimension as input layer
 			feedback should have the same dimension
-			of output layer
-		list predict(data) - apply model to data and
+			of output layer, [times] defines samples of data will 
+			be trained
+		list apply(data) - apply model to data and
 			predict the expected output
-		void test(data, expected) - use data to rate
-			this model, and print out correctness, it's
-			sort of validation
 	"""
 
 	def __init__(self, layers, learning_rate) :
 		self.layers = layers;
 		self.learning_rate = learning_rate;
+		
 		# constructing the network
-		if len(layers)<3 :
+		if len(self.layers)<3 :
 			print 'BPNN Layer setting error! At least 3 layers!'
 			return
-		self.inputLayerDim = layers[0]
-		self.outputLayerDim = layers[len(layers)-1]
-		self.hiddenLayerDim = layers[1:(len(layers)-1)]
+		self.inputLayerDim = self.layers[0]
+		self.outputLayerDim = self.layers[len(layers)-1]
+		self.hiddenLayerDim = self.layers[1:(len(layers)-1)]
+		
 		# allocate memory for connections
 		# input-hidden connections
 		self.ihc = np.arange(self.inputLayerDim)
 		for i in range(len(self.ihc)) :
-			self.ihc[i] = np.arange(self.hiddenLayerDim[0])
+			self.ihc[i] = np.random.ranf(self.hiddenLayerDim[0])
 		# there could be situation if ihconn allocation failed.
+		
 		# hidden-output-connections
 		self.hoc = np.arange(\
 		self.hiddenLayerDim[len(self.hiddenLayerDim)-1])
 		for i in range(len(self.hoc)) :
-			self.hoc[i] = np.arange(self.outputLayerDim)
+			self.hoc[i] = np.random.ranf(self.outputLayerDim)
+		
 		# connections between k to k+1 hidden layer
 		self.hhc = []
 		if len(self.hiddenLayerDim)>1 :
@@ -55,8 +57,31 @@ class BPNN(object) :
 				self.hhc[i] = np.arange(self.hiddenLayerDim[i])
 				for j in range(len(self.hhc[i])) :
 					self.hhc[i][j] = \
-					np.arange(self.hiddenLayerDim[i+1])
-
+					np.random.ranf(self.hiddenLayerDim[i+1])
+		
+		# allocation for input and output for each layer
+		self.x = np.arange(len(self.layers)-1)
+		self.y = np.arange(len(self.outputLayerDim))
+		self.e = np.arange(len(self.outputLayerDim))
+		for i in range(len(self.x)) :
+			self.x[i] = np.arange(len(self.layers[i]))
+		# over!
+	
+	def rand_train(data, feedback, times) :
+		if len(data[0]) != self.inputLayerDim :
+			print 'TRAINING INPUT DIMENSION DOES NOT \
+			MATCH WITH INPUT LAYER! PROGRAM EXIT WITH ERROR!'
+			return
+		if len(feedback[0]) != self.outputLayerDim :
+			print 'TRAINING FEEDBACK DIMENSION DOES NOT \
+			MATCH WITH OUTPUT LAYER! PROGRAM EXIT WITH ERROR!'
+		if len(feedback) != len(data) :
+			print 'FEEDBACK DIMENSION DOES NOT MATCH WITH INPUT!\
+			PROGRAM EXIT WITH ERROR!'
+		train_order = np.random.randint(len(data), times)
+		for i in train_order :
+			del self.x[0]
+			self.x[0] = data[train_order[i]]
 
 
 
